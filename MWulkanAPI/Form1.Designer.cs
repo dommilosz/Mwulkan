e92c7686cd007774f809707e21a12b0cc3118232
dommilosz <dommilosz@gmail.com>
2020-03-03 19:25:49 +0100

    Upload (Device Registration)

---
+++ MWulkanAPI/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MWulkanAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "bydgoszcz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ MWulkanAPI/MWAPI.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace MWulkanAPI
+{
+    public static class MWAPI
+    {
+        public static string token;
+        public static List<RootingRule> RootingRules;
+        public static void Test()
+        {
+            GetRoutingRules();
+            ResolveCode("3S1VBFJ");
+        }
+
+        public static string GetPFX(string code, string pin, string symbol)
+        {
+            RestClient client = new RestClient(new Uri($"{ResolveCode(code)}/{symbol}/mobile-api/Uczen.v3.UczenStart/Certyfikat"));
+            var request = new RestRequest();
+
+            int unixTimestamp = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            string devman = "Mwulkan";
+            string devmod = Environment.MachineName;
+            string uuid1 = Guid.NewGuid().ToString();
+            string uuid2 = Guid.NewGuid().ToString();
+            string content = $"{{ \"PIN\": \"{pin}\", \"TokenKey\": \"{code}\", \"AppVersion\": \"18.4.1.388\", \"DeviceId\": \"{uuid1}\", \"DeviceName\": \"{devman}#{devmod}\", \"DeviceNameUser\": \"\", \"DeviceDescription\": \"\", \"DeviceSystemType\": \"Android\", \"DeviceSystemVersion\": \"6.0.1\", \"RemoteMobileTimeKey\": {unixTimestamp}, \"TimeKey\": {unixTimestamp - 1}, \"RequestId\": \"{uuid2}\", \"RemoteMobileAppVersion\": \"18.4.1.388\", \"RemoteMobileAppName\": \"VULCAN-Android-ModulUcznia\" }} ";
+
+            var body = new RequestBody("application/json", "", content);
+            request.Body = body;
+            request.AddHeader("RequestMobileType", "RegisterDevice");
+            request.AddHeader("User-Agent", "MobileUserAgent");
+            request.AddHeader("Content-Type", "application/json");
+            var response = client.Post(request);
+            var json = response.Content;
+            try
+            {
+                var data = (JObject)JsonConvert.DeserializeObject(json);
+                var token2 = data["TokenCert"];
+                token = token2.ToString();
+                File.WriteAllText($"{Application.StartupPath}/token.tk",token.ToString());
+            }
+            catch { }
+            return "";
+        }
+        public static void GetRoutingRules()
+        {
+            RootingRules = new List<RootingRule>();
+            WebClient w = new WebClient();
+            var tmp = w.DownloadString("http://komponenty.vulcan.net.pl/UonetPlusMobile/RoutingRules.txt");
+            var tables = tmp.Split('\n').ToList();
+            for (int i = 0; i < tables.Count; i++)
+            {
+                tables[i] = tables[i].TrimEnd('\r');
+            }
+            foreach (var item in tables)
+            {
+                if (item.Split(',').Length > 1)
+                    RootingRules.Add(new RootingRule(item.Split(',')[0], item.Split(',')[1]));
+            }
+        }
+        public static string ResolveCode(string code)
+        {
+            GetRoutingRules();
+            string symbol = code[0].ToString() + code[1].ToString() + code[2].ToString();
+            foreach (var item in MWAPI.RootingRules)
+            {
+                if (item.code.Contains(symbol)) return item.url;
+            }
+            return null;
+        }
+    }
+    public class RootingRule
+    {
+        public string code;
+        public string url;
+        public RootingRule(string code, string url)
+        {
+            this.code = code;
+            this.url = url;
+        }
+    }
+}
